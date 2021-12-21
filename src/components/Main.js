eaabad92c69cb36167bf1b6cfe434e3b455219e6
Hongmin <hongmin@Hongminui-MacBookPro.local>
2021-12-21 14:27:40 +0900

    [fix] background size

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,8 +6,11 @@ import Logo from './Logo'
 
 class Main extends React.Component {
   render () {
+    const webWidth = window.innerWidth > 1440 ? window.innerWidth : 1440
+    const webHeight = window.innerHeight > 1024 ? window.innerHeight : 1024
+
     return (
-      <div className='main-background'>
+      <div className='main-background' style={{ width: `${webWidth}px`, height: `${webHeight}px` }}>
         <div className='title'>Container-Native Clusters for Robot Intelligence</div>
         <Logo />
         <ImageTable width='260' left='1120px' top='104px'/>
